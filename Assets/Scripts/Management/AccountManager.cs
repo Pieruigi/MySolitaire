1f32c3923b73c803715f6319685e1f4f44ebe5da
Pieruigi <pierluigi.zomparelli@gmail.com>
2021-12-27 16:58:55 +0100

    wip

--- Assets/Scripts/Management/AccountManager.cs
+++ Assets/Scripts/Management/AccountManager.cs
@@ -1,3 +1,4 @@
+using GooglePlayGames.BasicApi;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -51,23 +52,57 @@ namespace Zoca.Management
         void LogIn()
         {
 #if UNITY_ANDROID
-            GooglePlayGames.PlayGamesPlatform.Instance.Authenticate(GooglePlayGames.BasicApi.SignInInteractivity.CanPromptAlways, 
-                (result) => 
-                {
-                    if (result == GooglePlayGames.BasicApi.SignInStatus.Success)
-                        Logged = true;
-                    else
-                        Logged = false;
+            PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+                    // enables saving game progress.
+                    .EnableSavedGames()
+                    // requests the email address of the player be available.
+                    // Will bring up a prompt for consent.
+                    .RequestEmail()
+                    // requests a server auth code be generated so it can be passed to an
+                    //  associated back end server application and exchanged for an OAuth token.
+                    .RequestServerAuthCode(false)
+                    // requests an ID token be generated.  This OAuth token can be used to
+                    //  identify the player to other services such as Firebase.
+                    .RequestIdToken()
+                    .Build();
 
-                    
-                    Debug.Log("LogIn callback - Result:" + (GooglePlayGames.BasicApi.SignInStatus)result);
-                } );
+            GooglePlayGames.PlayGamesPlatform.InitializeInstance(config);
+            // recommended for debugging:
+            GooglePlayGames.PlayGamesPlatform.DebugLogEnabled = true;
+            // Activate the Google Play Games platform
+            GooglePlayGames.PlayGamesPlatform.Activate();
+
+            Social.Active.localUser.Authenticate((result) =>
+            {
+                if (result)
+                    Logged = true;
+                else
+                    Logged = false;
+
+
+                Debug.Log("LogIn callback - Result:" + result);
+            });
+            
+
+            //GooglePlayGames.PlayGamesPlatform.Instance.Authenticate(GooglePlayGames.BasicApi.SignInInteractivity.CanPromptAlways, 
+            //    (result) => 
+            //    {
+            //        if (result == GooglePlayGames.BasicApi.SignInStatus.Success)
+            //            Logged = true;
+            //        else
+            //            Logged = false;
+
+
+            //        Debug.Log("LogIn callback - Result:" + (GooglePlayGames.BasicApi.SignInStatus)result);
+            //    } );
 #endif
 
 #if UNITY_IOS
 
 #endif
         }
+
+        
 #endregion
 
 #region public methods
---
+++ Assets/Scripts/UI/ButtonLeaderboard.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zoca.Management;
+
+namespace Zoca.UI
+{
+    public class ButtonLeaderboard : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(HandleOnClick);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void HandleOnClick()
+        {
+            //if (!GooglePlayGames.PlayGamesPlatform.Instance.IsAuthenticated())
+            //{
+            //    return;
+            //}
+
+            LeaderboardManager.Instance.LoadScores();
+        }
+    }
+
+}
